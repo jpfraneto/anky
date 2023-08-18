ed1914d925cbc607693803a88e156b2772f953ce
jpfraneto <jpfraneto@gmail.com>
2023-08-18 16:27:42 -0400

    ahora si

--- pages/chocapec.js
+++ pages/chocapec.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { usePrivy } from '@privy-io/react-auth';
 
 const Chocapec = () => {
-  const { login } = usePrivy();
+  const { login, user } = usePrivy();
   return (
     <div>
       <button onClick={login}>privy login</button>
       <button onClick={() => alert('wena compare!')}>get info</button>
+      {user && (
+        <div>
+          <p>{user.wallet}</p>
+          <p>{user.id}</p>
+          <p>{user.createdAt}</p>
+          <p>{user.phone}</p>
+        </div>
+      )}
     </div>
   );
 };
