153e5fd8bfc2c0ee79248d2215a04ecbdf7052c1
jpfraneto <jpfraneto@gmail.com>
2023-08-20 17:01:07 -0400

    aloooja

--- components/AnswerToQuestionCard.js
+++ components/AnswerToQuestionCard.js
@@ -2,12 +2,19 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const AnswerToQuestionCard = ({ answer }) => {
+const AnswerToQuestionCard = ({ answer, index }) => {
   return (
-    <div className='w-full my-2 flex flex-row items-center  rounded-xl bg-gray-700 border-white border-2 shadow-orange-300 shadow-md'>
+    <div
+      onClick={() =>
+        alert(
+          'Grow this comment so that you can read it in here. If you tap again, it gets smaller.'
+        )
+      }
+      className='w-full my-2 flex flex-row items-center  rounded-xl bg-gray-700 border-white border-2 shadow-orange-300 shadow-md'
+    >
       <div className='w-1/4 flex items-center h-full justify-center '>
         <div className='w-11/12 aspect-square relative rounded-full m-2 overflow-hidden border-2 border-white'>
-          <Image src={`/ankys/${1}.png`} fill />
+          <Image src={`/ankys/${index + 1}.png`} fill />
         </div>
       </div>
 
--- pages/question/[id].js
+++ pages/question/[id].js
@@ -12,10 +12,10 @@ const QuestionById = () => {
     'But yesterday I realized that that can also be an experience of the present moment. The way on which he was deriving the most out of that moment, was through recording it with his phone, so that he could send it to his friends, or whatever he wanted to do with it. That IS a valid way of spending his time as we were singing her happy birthday. It IS the way that makes more sense to him, in that moment, and that is why he is doing it. Who am I to judge that? Who am I to blame? No one. It doesn’t matter. If I find more value just by being a witness and enjoying it without the meditation of technology, it is perfect like that. Just do it. But make sure you respect the way on which other people decide to do things also. This was a big eye opener yesterday, because now I realize how much I have judged other people because of doing something like this. And what is the use of it? Why do I even care? I can only control my own life experience, the things that I do, what I care about, and caring about what other person decides to do in a particular moment doesn’t help at all. It is just noise. And I want to break free of that noise. I want to integrate it fully, and move towards the rest of my life without it. It ends up being a burden. And I don’t want that burden on top of myself. I just want to be able to enjoy every second of my experience as a human being, to be present with what is, and if I’m there judging him because he is recording, or even worse, telling him to stop, I’m just being the asshole that I have been for a big part of my life.',
   ];
   return (
-    <div className='flex flex-col w-full px-4 mt-8 pb-24'>
+    <div className='flex flex-col w-full px-4 mt-12 pb-24'>
       <LandingQuestionCard
         question='What does personal transformation mean to you, and how have you pursued it?'
-        avatar='8'
+        avatar='anky'
         id='1'
       />
       {/* <div className='my-4 px-2'>
@@ -30,11 +30,15 @@ const QuestionById = () => {
         buttonColor='bg-green-700'
       /> */}
       {answers.map((answer, i) => (
-        <AnswerToQuestionCard answer={answer} key={i} />
+        <AnswerToQuestionCard answer={answer} key={i} index={i} />
       ))}
       <div className='mt-2'>
         <Button
-          buttonAction={() => alert('Open the WritingGame component')}
+          buttonAction={() =>
+            alert(
+              'Open the WritingGame component so that you can write in Anky in there.'
+            )
+          }
           buttonText='Write your answer'
           buttonColor='bg-green-700'
         />
