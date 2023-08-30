ac0f78b947adc41b8ae9c95b57fad785e2cdd4d8
jpfraneto <jpfraneto@gmail.com>
2023-08-30 06:41:17 -0400

    vamo

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
+const runtimeCaching = require('next-pwa/cache');
+
 const withPWA = require('next-pwa')({
-  webpack5: true,
   dest: 'public',
-  // disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true,
-  // scope: '/app',
-  // sw: 'service-worker.js',
-  //...
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
 });
 
 module.exports = withPWA({
