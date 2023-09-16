69d1ca61bf6f3f82d1ae849b4ab1d489383c2fb3
jpfraneto <jpfraneto@gmail.com>
2023-09-16 16:58:21 -0300

    aloja

--- components/MobileApp.js
+++ components/MobileApp.js
@@ -1,59 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const sections = [
   {
     question: 'why?',
-    image: '1.png',
-    oneLiner: 'because humanity is awakening.',
+    image: 'why.png',
+    oneLiner:
+      'because humanity is awakening and it is a hard process. like being born again but feeling that you are completely crazy.',
   },
   {
     question: 'what?',
-    image: '2.png',
+    image: 'what.png',
     oneLiner:
-      'an interface to develop practical tools for you to realize who you really are.',
+      'an interface to develop practical tools for you to realize who you really are. a network of humans collaborating in awesome open source software here: <a className="text-yellow-600" href="https://www.github.com/ankylat" target="_blank">https://www.github.com/ankylat</a>',
   },
   {
     question: 'when?',
-    image: '3.png',
-    oneLiner: 'forever on the blockchain.',
+    image: 'when.png',
+    oneLiner: 'forever on the blockchain. here. now.',
   },
   {
     question: 'where?',
-    image: '4.png',
-    oneLiner: 'on a small store in the downtown of your city.',
+    image: 'where.png',
+    oneLiner:
+      'on a small store in the city where you live. the coolest store in town.',
   },
   {
     question: 'who?',
-    image: '5.png',
+    image: 'who.png',
     oneLiner: 'those that are serious about the quest for truth.',
   },
   {
     question: 'how?',
-    image: '6.png',
-    oneLiner: 'by showing up.',
+    image: 'how.png',
+    oneLiner: 'by showing up. being who you already are. as simple as that.',
   },
 ];
 
 const MobileApp = () => {
   return (
     <div className='h-screen'>
-      {sections.map((x, i) => {
-        return (
-          <div
-            className='h-1/6 p-4 w-full relative bg-cover'
-            style={{
-              backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('/ankys/${x.image}')`,
-              backgroundPosition: 'center center',
-              backgroundSize: 'cover',
-              backgroundRepeat: 'no-repeat',
-            }}
-          >
-            <p className='text-white text-3xl'>{x.question}</p>
-          </div>
-        );
-      })}
+      {sections.map((x, i) => (
+        <Element section={x} key={i} />
+      ))}
     </div>
   );
 };
 
 export default MobileApp;
+
+const Element = ({ section }) => {
+  const [opened, setOpened] = useState(false);
+  console.log('the section is: ', section);
+  return (
+    <div
+      onClick={() => setOpened(x => !x)}
+      className='h-1/6 p-4 w-full relative bg-cover'
+      style={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('/ankys/${section.image}')`,
+        backgroundPosition: 'center center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
+      <p className='text-white text-3xl'>{section.question}</p>
+      {opened && (
+        <p
+          className='text-white'
+          dangerouslySetInnerHTML={{ __html: section.oneLiner }}
+        ></p>
+      )}
+    </div>
+  );
+};
