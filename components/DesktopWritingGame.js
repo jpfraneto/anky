2cce9a534d4affb46e646b8d192cfd4725842429
jpfraneto <jpfraneto@gmail.com>
2023-10-26 18:44:47 -0300

    updated the bug

--- components/DesktopWritingGame.js
+++ components/DesktopWritingGame.js
@@ -219,6 +219,7 @@ const DesktopWritingGame = ({
         await transactionResponse.wait(); // Wait for the transaction to be mined
         console.log('Notebook template created successfully');
         setSavingTextAnon(false);
+        setDisplayWritingGameLanding(false);
       } else {
         console.error('Wallet not connected or not authenticated with Privy');
       }
