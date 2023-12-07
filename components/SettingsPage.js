403cbd386bd2241c305c3eb541c05e06df8ae6ab
jp <jp@jps-MacBook-Air.local>
2023-12-07 08:23:41 -0300

    added settings comment

--- components/SettingsPage.js
+++ components/SettingsPage.js
@@ -4,8 +4,9 @@ const SettingsPage = () => {
   return (
     <div className="pt-4">
       <h2 className="text-white text-4xl">Settings</h2>
-      
-    </div>
+      <p className="mb-4">What settings should be added here?</p>
+      <p className="mb-4">Please help me figure it out by adding your feedback here: <a href='https://anky.canny.io' target='_blank'>https://anky.canny.io</a></p>
+    </div>  
   );
 };
 
