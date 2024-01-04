42e77359d9ec5ad947cdbcf16173433f3b87251c
jp <jp@jps-MacBook-Air.local>
2024-01-04 16:06:52 -0300

    ahora si

--- components/Leaderboard.js
+++ components/Leaderboard.js
@@ -1,29 +1,34 @@
 import React, { useEffect, useState } from "react";
 import Button from "./Button";
+import Spinner from "../components/Spinner";
 import Link from "next/link";
 import axios from "axios";
 
 const Leaderboard = () => {
   const [leaderboardData, setLeaderboardData] = useState([]);
+  const [loadingData, setLoadingData] = useState(false);
   const [activeLeaderboard, setActiveLeaderboard] = useState("all-time"); // all-time, today, longest-runs
   useEffect(() => {
-    console.log("now fetching the today", activeLeaderboard);
+    setLoadingData(true);
     fetchLeaderboardData(activeLeaderboard);
+    setLoadingData(false);
   }, [activeLeaderboard]);
+
   const fetchLeaderboardData = async (category) => {
     try {
       const response = await axios.get(
         `${process.env.NEXT_PUBLIC_API_ROUTE}/mana/leaderboard/${category}`
       );
-      console.log("the response from the db is: ', ", response);
+      console.log("the data is: ", response.data);
       setLeaderboardData(response.data);
     } catch (error) {
       console.error("Error fetching leaderboard data:", error);
       // Handle error state appropriately
     }
   };
+
   return (
-    <div className="container mx-auto flex flex-col px-4 py-2">
+    <div className="container mx-auto flex flex-col  px-4 py-2">
       <div className="flex justify-center mb-4">
         <button
           className={`px-4 py-2 rounded ${
@@ -56,33 +61,38 @@ const Leaderboard = () => {
           LONGEST SESSIONS
         </button>
       </div>
-
-      <div className="overflow-x-auto">
-        <table className="min-w-full bg-white border-collapse">
-          <thead>
-            <tr className="bg-gray-800 text-white">
-              <th className="px-4 py-2 border">User</th>
-              <th className="px-4 py-2 border">
-                {activeLeaderboard === "longest-runs"
-                  ? "Longest Run"
-                  : "Earned NEWEN"}
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {leaderboardData.map((entry, index) => (
-              <tr className="hover:bg-gray-100" key={index}>
-                <td className="px-4 py-2 border text-blue-500 hover:text-blue-700">
-                  <a href={`/u/${entry.userId}`}>{entry.userId}</a>
-                </td>
-                <td className="px-4 py-2 border">
-                  {entry._sum?.amount || entry.amount}
-                </td>
+      {loadingData ? (
+        <div>
+          <Spinner />
+        </div>
+      ) : (
+        <div className="overflow-x-auto">
+          <table className="min-w-full bg-white border-collapse">
+            <thead>
+              <tr className="bg-gray-800 text-white">
+                <th className="px-4 py-2 border">User</th>
+                <th className="px-4 py-2 border">
+                  {activeLeaderboard === "longest-runs"
+                    ? "Longest Run"
+                    : "Earned NEWEN"}
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {leaderboardData.map((entry, index) => (
+                <tr className="hover:bg-gray-100" key={index}>
+                  <td className="px-4 py-2 border text-blue-500 hover:text-blue-700">
+                    <Link href={`/u/${entry.userId}`}>{entry.userId}</Link>
+                  </td>
+                  <td className="px-4 py-2 border">
+                    {entry._sum?.amount || entry.amount}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
