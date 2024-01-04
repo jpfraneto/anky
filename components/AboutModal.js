ddefa1a08bb372f7937ec2ab8176ad64fab2f9c1
jp <jp@jps-MacBook-Air.local>
2024-01-04 14:23:35 -0300

    aloja

--- components/AboutModal.js
+++ components/AboutModal.js
@@ -3,8 +3,8 @@ import React, { useState, useEffect } from "react";
 const AboutModal = () => {
   return (
     <div>
-      <div className="absolute bg-black w-screen h-screen opacity-50 top-0 left-0"></div>
-      <div className="absolute text-left pt-4 top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 bg-black h-3/5 rounded-xl w-96 z-40 mt-4 text-white overflow-y-scroll px-4 py-4">
+      <div className="fixed bg-black w-screen h-screen opacity-50 top-0 left-0"></div>
+      <div className="fixed text-left pt-4 top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 bg-black h-3/5 rounded-xl w-96 z-40 mt-4 text-white overflow-y-scroll px-4 py-4">
         <h2 className="text-2xl">About Anky</h2>
         <p className="mb-2">
           first of all, welcome. it is great to have you here. here are some
@@ -32,10 +32,10 @@ const AboutModal = () => {
             <span className="text-purple-600">$NEWEN:</span> This is a currency
             that represents time. For each second that you spend writing in
             here, you will earn it. You will then be able to spend it on
-            different ways: "super liking" a cast you&apos;ve read by sending
-            newen to the writer, attending events that before were "free", but
-            now require something that is "free" (your time writing here), but
-            doesn&apos;t have economical value.
+            different ways: &quot;super liking&quot; a cast you&apos;ve read by
+            sending newen to the writer, attending events that before were
+            &quot;free&quot;, but now require something that is &quot;free&quot;
+            (your time writing here), but doesn&apos;t have economical value.
           </li>
           <li>
             <span className="text-purple-600">$ANKY:</span> This is the
