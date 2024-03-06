51d500f345e93f7de48ae5fffcb62064cb575878
jp <jp@jps-MacBook-Air.local>
2024-03-06 15:30:57 -0300

    aloja

--- components/AnkyWritersIndexPage.js
+++ components/AnkyWritersIndexPage.js
@@ -257,6 +257,8 @@ const placeholderWriters = [
 const AnkyWritersIndexPage = () => {
   const [ankyWriters, setAnkyWriters] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [email, setEmail] = useState("");
+  const [submittedEmail, setSubmittedEmail] = useState(false);
 
   useEffect(() => {
     const fetchAllAnkyWriters = async () => {
@@ -275,6 +277,27 @@ const AnkyWritersIndexPage = () => {
     fetchAllAnkyWriters();
   }, []);
 
+  function isValidEmail(email) {
+    const regex =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@(([^<>()[\]\\.,;:\s@"]+\.)+[^<>()[\]\\.,;:\s@"]{2,})$/i;
+    return regex.test(email);
+  }
+
+  async function sendEmail() {
+    try {
+      if (!email || !isValidEmail(email)) return alert("add a valid email");
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_ROUTE}/add-email`,
+        {
+          email: email,
+        }
+      );
+      setSubmittedEmail(true);
+    } catch (error) {
+      alert("there was an error adding your email, please try again");
+    }
+  }
+
   return (
     <div className="w-full md:px-32">
       <div className="md:w-2/3 text-white  mt-2 mx-auto">
@@ -282,20 +305,29 @@ const AnkyWritersIndexPage = () => {
           anky writers · mint live · 10 march 2024 · 5 am EST
         </h2>
         <p className="text-white px-8 mb-2 text-white">
-          submit your email to get yours and participate on{" "}
-          <a
-            className="text-blue-300 hover:text-yellow-600"
-            href="https://warpcast.com/jpfraneto/0x9664dfc0"
-            target="_blank"
-          >
-            this farcaster frame
-          </a>
-          . they will be free.
+          submit your email to get yours and participate on this journey. they
+          will be free.
         </p>
         <p className="text-white px-8 mb-2 text-white">
           the mission: write every day for 8 minutes during 96 days. without
           judgements. only exploring what comes.
         </p>
+        {submittedEmail ? (
+          <p className="text-white">your email was submitted</p>
+        ) : (
+          <div>
+            <input
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+              className="p-2 w-96 mx-auto"
+            />
+            <Button
+              buttonText="submit"
+              buttonAction={sendEmail}
+              buttonColor="bg-purple-600 text-white"
+            />
+          </div>
+        )}
       </div>
 
       {!loading ? (
